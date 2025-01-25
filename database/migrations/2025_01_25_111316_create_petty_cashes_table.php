83b04ff43afbee6d94c172b6cd4ef1be0b35eb20
shakil <kazimuhammadullah@gmail.com>
2025-01-25 17:17:23 +0600

    petty cash

---
+++ database/migrations/2025_01_25_111316_create_petty_cashes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('petty_cashes', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->nullable();
+            $table->string('count')->nullable();
+            $table->double('amount', 12, 2)->nullable();
+            $table->boolean('status')->default(1); 
+            // 1= new or processing, 0= cancel, 2=hold, 3=complete 
+            $table->string('updated_by')->nullable();
+            $table->string('created_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('petty_cashes');
+    }
+};
