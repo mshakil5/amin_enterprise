9644cc0be8569d88d9181ecd5e1f337c76fe656a
shakil <46122677+mshakil5@users.noreply.github.com>
2026-01-07 20:41:36 +0600

    issue fix

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -15,7 +15,7 @@ class AccountController extends Controller
     {
         $data = Account::latest()->get();
         $allAccounts = Account::latest()->get();
-        $transactions = Transaction::whereIn('tran_type',['TransferIn', 'TransferOut'])->select('date', 'id','amount','tran_id', 'description','payment_type','tran_type', 'at_amount')->get();
+        $transactions = Transaction::whereIn('tran_type',['TransferIn', 'TransferOut'])->select('date', 'id','amount','tran_id', 'description','payment_type','tran_type', 'at_amount')->latest()->get();
         
         return view('admin.account.index', compact('data', 'allAccounts','transactions'));
     }
